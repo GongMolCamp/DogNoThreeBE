2f2b57df3d4fccd218ee20848e917b7618b0a2ae
김태윤 <77539625+pykido@users.noreply.github.com>
2024-12-23 00:47:08 +0900

    Feature1 (#6)
    * docs : gitignore 설정
    
    * docs : gitignore 설정
    
    * docs : gitignore 설정
    
    * test
    
    * Update .gitignore (#5)
    
    * test

--- src/main/java/gongnon/App.java
+++ src/main/java/gongnon/App.java
@@ -5,7 +5,7 @@ package gongnon;
 
 public class App {
     public String getGreeting() {
-        return "Helldo World!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
