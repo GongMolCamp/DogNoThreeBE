e9724c06a89749b37e2551a8af7ce6635adfb2c4
pykido <animoto1@naver.com>
2024-12-23 00:59:39 +0900

    yml 파일 추가

--- src/main/java/gongnon/App.java
+++ src/main/java/gongnon/App.java
@@ -5,7 +5,7 @@ package gongnon;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello GongNon!!";
     }
 
     public static void main(String[] args) {
