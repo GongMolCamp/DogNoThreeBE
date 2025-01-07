395e7fff30c7dc6dac93e896b6eff1c0580f04f9
pykido <animoto1@naver.com>
2025-01-07 23:49:45 +0900

    feat : @SpringbootApplication 설정 완료!

---
+++ src/main/java/gongnon/GongNonApplication.java
@@ -0,0 +1,14 @@
+package gongnon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongNonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongNonApplication.class, args);
+		System.out.println("GongNon Application is running...");
+		System.out.println("haha!");
+	}
+}
